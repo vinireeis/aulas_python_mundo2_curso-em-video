4941d32d99aed53834b08614d66f1472d8c16c9f
Vinicius Dos Reis Oliveira <v_reis@outlook.com.br>
2021-06-01 00:13:51 -0300

    att
    exercicios

--- Aula01/exer50.py
+++ Aula01/exer50.py
@@ -1,11 +1,22 @@
 '''Leia seis números inteiros e mostre a soma apenas daqueles que forem pares.
 Se o valor for ímpar desconsidere-o'''
-lista = []
+
+soma = 0
 for r in range(1, 6+1):
     x = int(input('Informe um valor: '))
     if x % 2 == 0:
+        soma += x
+print(soma)
+
+
+'''
+lista = []
+soma = 0
+for n in lista:
+    for r in range(1, 6+1):
+    x = int(input('Informe um valor: '))
+    if x % 2 == 0:
         lista.append(x)
-y = 0
-for r in lista:
-    y += r
-print(r)
+for n in lista:
+    soma += n
+print(soma)'''
