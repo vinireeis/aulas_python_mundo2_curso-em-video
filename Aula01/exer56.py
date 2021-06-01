d13439b6de29b1c0e8ca6ddb144b2e9a8d3d638f
Vinicius Dos Reis Oliveira <v_reis@outlook.com.br>
2021-06-01 15:19:58 -0300

    att dir

--- Aula01/exer56.py
+++
@@ -1,7 +0,0 @@
-'''Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas. No final do programa, mostre:
-- A média de idade do grupo
-- Qual o nome do homem mais velho
-- Quantas mulheres têm menos de 20 anos
-
-
-
