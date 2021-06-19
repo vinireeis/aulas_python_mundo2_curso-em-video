1952a9e8c398fdf3a9837f699488d18e51c97094
Vinicius Reis <v_reis@outlook.com.br>
2021-06-19 12:49:43 -0300

    começando while

---
+++ while.py
@@ -0,0 +1,16 @@
+'''for c in range(1, 10):
+    print(c)'''
+
+c = 1
+while c < 10:
+    print(c)
+    c += 1
+print('Fim')
+print('-' * 60)
+n = 1
+while n != 0:
+    n = int(input('para parar digite 0 ou digite um novo valor: '))
+    print('-' * 15)
+    print(n)
+    print('-' * 15)
+print('Fim')
