438008e022fa2947a3a8f2f743f75adc3f54ebac
Vinicius Reis <v_reis@outlook.com.br>
2021-06-19 15:07:42 -0300

    desafio 59

---
+++ exer58jogoAdvinheOqueEstouPensando.py
@@ -0,0 +1,19 @@
+''' escreva um programa que faça o computador "pensar" em um número inteiro
+    entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número
+    escolhido pelo computador'''
+import random
+
+n = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+sort = random.choice(n)
+print('-' * 60)
+cont = 0
+n1 = int(input('O computador está pensando em um número de 0 a 10, digite um'
+               ' número para saber se acertou o mesmo valor: '))
+print('-' * 60)
+while sort != n1:
+    n1 = int(input('Você errou, tente novamente acertar digitando'
+                   ' um novo número: '))
+    cont += 1
+    print('-' * 60)
+print(f'Parabéns o computador pensou no {sort}, sendo'
+      f' que você precisou de {cont} tentativas para acertar.')
---
+++ exer59MenuCalc.py
@@ -0,0 +1,27 @@
+n = 4
+valor1 = float(input('Informe o primeiro valor: '))
+valor2 = float(input('Informe o segundo valor: '))
+while n != 5:
+    n = int(input(('-' * 60) + '\nEscolha uma das opções abaixo: \n[1]SOMAR'
+                  '\n[2]MULTIPLICAR\n[3]MAIOR\n[4]NOVOS NUMEROS\n[5]SAIR\n' +
+                  '-' * 60 + '\n'))
+    if n == 1:
+        x = valor1 + valor2
+        print(f'O resultado da soma é {x}')
+    elif n == 2:
+        x = valor1 * valor2
+        print(f'O resultado da multiplicação é {x}')
+    elif n == 3:
+        if valor1 > valor2:
+            print(f'O maior valor é {valor1}')
+        elif valor1 == valor2:
+            print('Os dois valores são equivalentes')
+        else:
+            print(f'O maior valor é {valor2}')
+    elif n == 4:
+        valor1 = float(input('Informe o primeiro valor: '))
+        valor2 = float(input('Informe o segundo valor: '))
+    else:
+        print('-' * 60)
+        print('Digite uma opção correta')
+        print('-' * 60)
