ab98c46d5d700b9df9e505d63f84fd2807367458
Vinicius Dos Reis Oliveira <v_reis@outlook.com.br>
2021-07-26 15:46:38 -0300

    exer66 usando break

---
+++ exer66-exer65+break.py
@@ -0,0 +1,13 @@
+n = 0
+lista = []
+result = 0
+
+while n != 999:
+    n = int(input("Digite um novo número: "))
+    if n != 999:
+        lista.append(n)
+    else:
+        break
+for i in lista:
+    result += i
+print(f'foram digitados {len(lista)} números e a soma total é {result}')
