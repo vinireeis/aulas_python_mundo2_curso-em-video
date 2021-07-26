8eb323476ee8641589f6c5845b2ff7fa09f02a95
Vinicius Dos Reis Oliveira <v_reis@outlook.com.br>
2021-07-26 15:33:24 -0300

    correcao exer65

--- exer65-ListaMediaMaiorMenor.py
+++ exer65-ListaMediaMaiorMenor.py
@@ -2,6 +2,9 @@ from statistics import mean
 
 r = 'S'
 lista = []
+x = int(input("Digite um número: "))
+lista.append(x)
+
 while r != 'N':
     r = str(input('Deseja digitar um valor? [S/N]')).upper()
     if r == 'S':
@@ -13,6 +16,10 @@ while r != 'N':
         r = str(input(
             'Digite uma resposta válida para continuar "s" para Sim e "n" para'
             'Não [S/N]:')).upper()
+media = mean(lista)
+print('-' * 60)
+print(f'Você digitou {len(lista)} números')
 print(
     f'A média dos valores digitados é {media:.2f}, já o maior valor é '
     f'{max(lista)} e o menor {min(lista)}.')
+print('-' * 60)
