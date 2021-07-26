0961109901a03094013cf517050c1640f6e33b9c
Vinicius Dos Reis Oliveira <v_reis@outlook.com.br>
2021-07-26 01:44:52 -0300

    segunda resolução

--- exer57-while.py
+++ exer57-while.py
@@ -7,3 +7,8 @@ if sexo == 'M':
 elif sexo == 'F':
     print('-' * 30)
     print('Você é do sexo Feminino')
+
+# sexo = input('Informe o seu sexo [M/F]: ').strip().upper()
+# while sexo not in 'MmFf':
+#     sexo = input('Informe corretamente o seu sexo [M/F]: ').strip().upper()
+# print(f'Sexo {sexo} registrado com sucesso')
