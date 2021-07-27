99d9ee5629eeab8011baf6506e998fb4e42e228b
Vinicius Dos Reis Oliveira <v_reis@outlook.com.br>
2021-07-26 21:34:30 -0300

    exer66 att

--- exer66-exer65+break.py
+++ exer66-exer65+break.py
@@ -1,13 +1,10 @@
 n = 0
 lista = []
-result = 0
 
 while n != 999:
-    n = int(input("Digite um novo número [999 encerrar]: "))
+    n = int(input("Digite um novo número ou digite 999 encerrar: "))
     if n != 999:
         lista.append(n)
     else:
         break
-for i in lista:
-    result += i
-print(f'foram digitados {len(lista)} números e a soma total é {result}')
+print(f'foram digitados {len(lista)} números e a soma total é {sum(lista)}')
